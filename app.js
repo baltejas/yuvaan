cdc3f48fc75d0d389720058a06d429733e071895
Tejas Bal <tejas.bal@outlook.com>
2021-03-13 23:32:26 -0500

    app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var path = require('path');
 const app = express();
-const port = 3000;
+var port = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
