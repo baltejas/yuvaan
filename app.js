b315d6757ff3b125399f0cfb9db4038057da8887
Tejas Bal <tejas.bal@outlook.com>
2021-03-14 13:33:56 -0400

    newletter

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 var path = require('path');
 const app = express();
 var port = process.env.PORT || 8080;
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/home.html'));
+});
+
+app.get('/newsletter', function(req, res) {
+  res.sendFile(path.join(__dirname + '/newsletter.html'));
 });
 
 app.listen(port, () => {
