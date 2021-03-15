30c7c8afa58514334d96e6668724c3e3f4af902a
Tejas Bal <tejas.bal@outlook.com>
2021-03-14 21:32:37 -0400

    updates

--- app.js
+++ app.js
@@ -2,14 +2,38 @@ const express = require('express');
 var path = require('path');
 const app = express();
 var port = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/home.html'));
+    res.sendFile(path.join(__dirname + '/pages/home.html'));
 });
 
 app.get('/newsletter', function(req, res) {
-  res.sendFile(path.join(__dirname + '/newsletter.html'));
+  res.sendFile(path.join(__dirname + '/pages/newsletter.html'));
+});
+
+app.get('/projects', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/projects.html'));
+});
+
+app.get('/youth-center', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/youth-emp-center.html'));
+});
+
+app.get('/career-development', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/career-development.html'));
+});
+
+app.get('/volunteer', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/volunteer.html'));
+});
+
+app.get('/impact', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/impact.html'));
+});
+
+app.get('/about-us', function(req, res) {
+  res.sendFile(path.join(__dirname + '/pages/about-us.html'));
 });
 
 app.listen(port, () => {
