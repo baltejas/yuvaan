fc3cbbb0200f69831ebe6aaed83c1abeb799c797
Tejas Bal <tejas.bal@outlook.com>
2021-03-27 19:01:48 -0400

    push

--- app.js
+++ app.js
@@ -1,49 +1,13 @@
-const express = require('express');
-var path = require('path');
+const express = require("express");
+var path = require("path");
 const app = express();
 var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/landing.html'));
-});
-
-app.get('/landing', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/landing.html'));
-});
-
-app.get('/newsletter', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/newsletter.html'));
-});
-
-app.get('/projects', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/projects.html'));
-});
-
-app.get('/youth-center', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/youth-emp-center.html'));
-});
-
-app.get('/career-development', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/career-development.html'));
-});
-
-app.get('/volunteer', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/volunteer.html'));
-});
-
-app.get('/impact', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/impact.html'));
-});
-
-app.get('/about-us', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/about-us.html'));
-});
-
-app.get('/donate', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/donate.html'));
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname + "/pages/landing.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Yuvaan app listening at http://localhost:${port}`)
-})
+	console.log(`Yuvaan app listening at http://localhost:${port}`);
+});
